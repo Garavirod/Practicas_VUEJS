5c2acc2a537589f26213414faf91f4cfc9697e23
Garavirod <rodrigogarciaavila26@gmail.com>
2020-02-12 21:38:13 -0600

    VUEjs v-for

---
+++ class1/01.js
@@ -0,0 +1,17 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        titulo: 'Hello world with VUEjs',
+        fruts: ['Pear', 'Watermelon', 'Orange', 'Apple'],
+        cars: [
+            { nameCar: 'Mazda', quantity: 10 },
+            { nameCar: 'Kia', quantity: 10 },
+            { nameCar: 'Mustang', quantity: 10 },
+            { nameCar: 'Chevelle', quantity: 10 },
+            { nameCar: 'Corla 86', quantity: 10 }
+        ]
+
+    }
+
+
+});
