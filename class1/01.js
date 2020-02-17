b625782e7c803aa7818edc9acd237ae94c1e3262
Garavirod <rodrigogarciaavila26@gmail.com>
2020-02-16 20:01:02 -0600

    Models and events

--- class1/01.js
+++ class1/01.js
@@ -9,6 +9,14 @@ const app = new Vue({
             { nameCar: 'Mustang', quantity: 0 },
             { nameCar: 'Chevelle', quantity: 10 },
             { nameCar: 'Corla 86', quantity: 10 }
-        ]
-    }
+        ],
+        newCar: ''
+    },
+    methods: {
+        addingCars() {
+            console.log("you've cliked addingCars Method !!");
+            // With 'this' we can access to many properties on the same instance
+            this.cars.push({ nameCar: this.newCar, quantity: 0 });
+        }
+    },
 });
