9656c7eca4f668f0a88f71f75d3c586ffa46fec5
Garavirod <rodrigogarciaavila26@gmail.com>
2020-03-19 13:00:17 -0600

    map mutations

--- Practice_7_vuex/components/child.js
+++ Practice_7_vuex/components/child.js
@@ -3,13 +3,20 @@ Vue.component('child', {
         <div class="bg-primary p-3 text-white m-3">
             <h3>Im the son and shared number is {{sharedNumber2}}</h3>
             <p>Other data from store : <i>'{{sharedData}}'</i></p>
-            <button class="btn btn-danger" @click="$store.commit('increase')">+</button>
+            <button class="btn btn-danger" @click="increase">+</button>
+            <button class="btn btn-dark" @click="less">-</button>
+            <button class="btn btn-success" @click="lessN(3)"> - 3</button>
         </div>
     
     `,
     computed: {
         //W're mapping all 'state' of the store.
         //Inside of array goes all you want to use of 'state' in this component
+        //DESTRUCTURATION
         ...Vuex.mapState(['sharedNumber2', 'sharedData']) //In this case we are going to use 'sharedNumber2' in our tamplate
+    },
+    //We can mapping the mutattions too using destructuration
+    methods: {
+        ...Vuex.mapMutations(['increase', 'less', 'lessN'])
     },
 });
