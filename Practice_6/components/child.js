6744f92aeac7afa712eff513d2a46b295397fe3a
Garavirod <rodrigogarciaavila26@gmail.com>
2020-03-18 23:50:49 -0600

    comunication from child to parent

--- Practice_6/components/child.js
+++ Practice_6/components/child.js
@@ -3,7 +3,18 @@ Vue.component('child', {
         <div class="bg-warning text-white p-5">
         <!--al ser un bind el dato que recibe es dinamico cambia desde el padre hasta el hijo-->
             <h4>I'm the child and my dad says: <i>{{message}}</i></h4>
+            <h2>{{sonMessage}}</h2>
         </div>
     `,
-    props: ['message'] //This is for recive information from other compoennt outside
+    props: ['message'], //This is for recive information from other compoennt outside
+    data() {
+        return {
+            sonMessage: 'I need money'
+        }
+    },
+    //it is executed when whole DOM and it's data have been loaded
+    mounted() {
+        // Let's emmit an event
+        this.$emit('messageFromSon', this.sonMessage); //'message' can be any name you want
+    },
 });
--- Practice_6/components/parent.js
+++ Practice_6/components/parent.js
@@ -3,13 +3,17 @@ Vue.component('parent', {
         <div class="p-5 bg-dark text-white">            
             <h2>This is the parent: {{dadMessage}}</h2>
             <button class="btn btn-danger" @click="dadMessage++">push me</button>
+            <h4>My son says : {{sonMessage}}</h4>
             <!--se ponen los puntos porque es un bind (propiedad del componente), de lo contrario se quitan-->
-            <child :message="dadMessage"></child>
+            <!--We recive an event using @ we called the event-->
+            <!--sobreescribumos el valor de la data del padre, en ese caso sonMess con lo que $vent retorne-->
+            <child :message="dadMessage" @messageFromSon="sonMessage = $event"></child>
         </div>    
     `,
     data() { //Data propoerties of the component
         return {
-            dadMessage: 0
+            dadMessage: 0,
+            sonMessage: ''
         }
     },
 });
